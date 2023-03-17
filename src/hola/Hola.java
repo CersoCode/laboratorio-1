5ec009196513d805bf16a74285a681804d59d181
vicecers <46388534+vicecers@users.noreply.github.com>
2023-03-17 17:43:40 -0300

    update main

--- src/hola/Hola.java
+++ src/hola/Hola.java
@@ -1,12 +1,12 @@
 package hola;
 /**
  * La clase hola construye un programa que visualiza el mensaje
- * "hola, me llamo Angel" en el dispositivo de salida estandar
+ * "hola, me llamo ..." en el dispositivo de salida estandar
  */
 public class Hola {
 
     public static void main(String[] args) {
         // Visualiza por pantalla "Hola, me llamo Angel"
-        System.out.println("Hola, me llamo Angel");
+        System.out.println("Hola, me llamo ...");
     }
 }
